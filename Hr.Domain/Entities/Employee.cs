173a9e7f2df749166231ae536c3948eae70ba72a
Karim Mahdy <karimmahdy43@gmail.com>
2023-11-05 21:36:35 +0200

    update appsetting

--- Hr.Domain/Entities/Employee.cs
+++ Hr.Domain/Entities/Employee.cs
@@ -31,7 +31,8 @@ namespace Hr.Domain.Entities
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
         public ICollection<Attendance>? Attendance { get; set; }
+        public ICollection<GeneralSettings>? GeneralSettings { get; set; }
 
-        
+
     }
 }
